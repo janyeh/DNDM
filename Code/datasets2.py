23990d5818bdb8d4493d7e44828732098f0393be
janyeh <janyeh@gmail.com>
2025-03-20 10:10:57 +0800

    UPDATE: (datasets2.py) make image list A the same size of B;

--- Code/datasets2.py
+++ Code/datasets2.py
@@ -313,6 +313,7 @@ class TrainDatasetFromFolder2(Dataset):
         self.image_filenames_A = [join(dataset_dir_c, x) for x in natsort.natsorted(listdir(dataset_dir_c))[0:1399] for p in range(10) if is_image_file(x)]
         self.image_filenames_B = [join(dataset_dir_h, x) for x in natsort.natsorted(listdir(dataset_dir_h))[0:13990]  if is_image_file(x)]
         self.image_filenames_C = [join(dataset_real, x) for x in natsort.natsorted(listdir(dataset_real))[0:13990]   if is_image_file(x)]
+        self.image_filenames_A = self.image_filenames_A[:len(self.image_filenames_B)]
 
         # self.image_filenames_A = [join(dataset_dir_c, x) for x in natsort.natsorted(listdir(dataset_dir_c))[0:2061] for p in range(7) if is_image_file(x)]   # for p in range(10)#5687    27327   2000  #is_image_file(10*x)] [0:5687]
         # self.image_filenames_B = [join(dataset_dir_h, x) for x in natsort.natsorted(listdir(dataset_dir_h))[0:14427] if is_image_file(x)]  #[0:5687]  54600
